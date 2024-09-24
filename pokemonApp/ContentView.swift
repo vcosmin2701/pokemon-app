8bd54d685f54388989e979b8a542c13338936811
vcosmin2701 <v.cosmin2717@gmail.com>
2024-09-24 10:27:06 +0300

    [implementation] - new version of GraphQL implementation

--- pokemonApp/ContentView.swift
+++ pokemonApp/ContentView.swift
@@ -6,16 +6,46 @@
 //
 
 import SwiftUI
+import Apollo
+import PokeAPI
+
 
 struct ContentView: View {
+    let apolloClient = ApolloClient(
+        url: URL(string: "https://beta.pokeapi.co/graphql/v1beta")!
+    )
+    
+    @State private var pokemonNames: [String] = []
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            List(pokemonNames, id: \.self) { name in
+                Text(name)
+            }
         }
         .padding()
+        .onAppear {
+            fetchPokemon()
+        }
+    }
+    
+    private func fetchPokemon() {
+        let query = PokemonGraphQLCallQuery()
+            
+        apolloClient.fetch(query: query) { result in
+            switch result {
+            case .success(let graphQLResult):
+                if let pokemons = graphQLResult.data?.pokemon_v2_pokemon {
+                    self.pokemonNames = pokemons.compactMap { $0.name }
+                }
+            case .failure(let error):
+                print("Error loading Pokémon: \(error.localizedDescription)")
+            }
+        }
     }
 }
 
