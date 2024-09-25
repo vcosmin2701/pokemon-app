abd28d1766fc82e179926a13efd9a737c0c8c3c7
vcosmin2701 <v.cosmin2717@gmail.com>
2024-09-25 14:16:41 +0300

    [refactor] - Code cleanup on DetailsViewModel

--- pokemonApp/Features/Details/DetailsViewModel.swift
+++ pokemonApp/Features/Details/DetailsViewModel.swift
@@ -2,35 +2,17 @@ import Foundation
 
 class DetailsViewModel: ObservableObject {
     @Published var pokemon: Pokemon
-    
     @Published var favPokemons: [Pokemon] = []
-    let defaults = UserDefaults.standard
     
-    func storePokemon(){
-        if let encoded = try? JSONEncoder().encode(favPokemons) {
-            defaults.set(encoded, forKey: "SavePokemon")
-        }
-    }
-    
-    func addFavorite(pokemon: Pokemon){
-        favPokemons.append(pokemon)
-        storePokemon()
-    }
-    
-    func deleteFavorite(pokemon: Pokemon){
-        if let index = favPokemons.firstIndex(where: {$0.id == pokemon.id}) {
-            favPokemons.remove(at: index)
-        }
-        storePokemon()
-    }
-    
-    var isFavoritePokemon: Bool {
-        return favPokemons.contains(where: { $0.id == pokemon.id})
-    }
+    private let defaults = UserDefaults.standard
     
     init(pokemon: Pokemon) {
         self.pokemon = pokemon
         loadFavorites()
+    }
+    
+    var isFavoritePokemon: Bool {
+        return favPokemons.contains(where: { $0.id == pokemon.id})
     }
     
     func toggleFavorite() {
@@ -41,11 +23,29 @@ class DetailsViewModel: ObservableObject {
         }
     }
     
-    func loadFavorites() {
+    private func loadFavorites() {
         if let data = defaults.data(forKey: "SavePokemon") {
             if let decoded = try? JSONDecoder().decode([Pokemon].self, from: data) {
                 favPokemons = decoded
             }
         }
+    }
+    
+    private func storePokemon(){
+        if let encoded = try? JSONEncoder().encode(favPokemons) {
+            defaults.set(encoded, forKey: "SavePokemon")
+        }
+    }
+    
+    private func addFavorite(pokemon: Pokemon){
+        favPokemons.append(pokemon)
+        storePokemon()
+    }
+    
+    private func deleteFavorite(pokemon: Pokemon){
+        if let index = favPokemons.firstIndex(where: {$0.id == pokemon.id}) {
+            favPokemons.remove(at: index)
+        }
+        storePokemon()
     }
 }
