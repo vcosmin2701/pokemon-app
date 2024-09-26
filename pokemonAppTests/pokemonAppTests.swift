294e3c9ed08b6a90d5872eb41c6a4dbc467c4b52
vcosmin2701 <v.cosmin2717@gmail.com>
2024-09-26 12:17:23 +0300

    [testing] - Add to favorite test

--- pokemonAppTests/pokemonAppTests.swift
+++ pokemonAppTests/pokemonAppTests.swift
@@ -1,14 +1,13 @@
 //
-//  pokemonAppTests.swift
-//  pokemonAppTests
+//  PokemonAppTests.swift
+//  PokemonAppTests
 //
-//  Created by Anonim on 23.09.2024.
+//  Created by Anonim on 26.09.2024.
 //
 
 import XCTest
-@testable import pokemonApp
 
-final class pokemonAppTests: XCTestCase {
+final class PokemonAppTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -28,7 +27,7 @@ final class pokemonAppTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
